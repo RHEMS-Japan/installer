204aa0a1f5d235f6ea9e7fec35ab3e9489598333
rayman <rayman@rhems-japan.co.jp>
2021-09-07 00:52:05 +0900

    Update install.sh
    rayman did

--- escaper/install.sh
+++ escaper/install.sh
@@ -2,7 +2,7 @@
 
 echo "RHEMS escaper!"
 
-wget -O - https://github.com/RHEMS-Japan/installer/raw/main/escaper/main.tar.gz | tar -zxv
+curl -OL https://github.com/RHEMS-Japan/installer/raw/main/escaper/main.tar.gz | tar -zxv
 cd main
 
 make
